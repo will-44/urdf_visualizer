87877cd17f288d7c75c53dea06d9be79306f0c19
will-44 <guillaume.dupoiron@gmail.com>
2024-10-17 19:29:59 -0400

    visualize valid for xacro and urdf. file for ridgeback and structure

--- launch/urdf_vizualiser.launch.py
+++ launch/urdf_vizualiser.launch.py
@@ -1,30 +1,61 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
 import os
+import xacro
 
 def launch_setup(context, *args, **kwargs):
-    # Get the URDF file name from the launch argument
-    urdf_filename = LaunchConfiguration('urdf_filename').perform(context)
+    
+    declare_xacro_file_cmd = DeclareLaunchArgument(
+        'filename',
+        default_value='robot.xacro',
+        description='Nom du fichier Xacro dans le dossier urdf')
 
+    filename = LaunchConfiguration('filename')#.perform(context)
+ 
     # Get the package share directory
     package_share_directory = get_package_share_directory('urdf_visualizer')
 
     # Construct the full path to the URDF file
-    urdf_file_path = os.path.join(package_share_directory, 'urdf', urdf_filename)
+    file_path = os.path.join(package_share_directory, 'urdf', filename.perform(context))
+    
+    if file_path.endswith('.xacro'):
+        # robot_description_content = Command(command=[
+        # "python3 -c '\n"
+        # "import yaml \n"
+        # "with open(\"", file_path, "\") as f: \n"
+        # "    print(yaml.dump(f.read()))"
+        # "'"
+        # ])
+        robot_description_content = Command(['xacro ', file_path])
+        # robot_description_content = xacro.process_file(robot_description_content).toxml()
+        print(robot_description_content)
+    elif file_path.endswith('.urdf'):
+        # Read the URDF file
+        with open(file_path, 'r') as infp:
+            robot_description_content = infp.read()
+            print(robot_description_content)
+    else:
+        # print error log ros2
+        print("Error: file extension is not xacro or urdf")
 
-    # Read the URDF file
-    with open(urdf_file_path, 'r') as infp:
-        robot_description_content = infp.read()
-
+    # robot_description = {'robot_description': ParameterValue(robot_description_content, value_type=str)}
     # Create the robot_state_publisher node
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': robot_description_content}],
+        parameters=[{'robot_description': ParameterValue(robot_description_content, value_type=str)}],
+    )
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_node',
+        emulate_tty=True,
+        output="screen"
     )
 
     # Create the RViz2 node
@@ -35,7 +66,7 @@ def launch_setup(context, *args, **kwargs):
         output='screen',
     )
 
-    return [robot_state_publisher_node, rviz_node]
+    return [robot_state_publisher_node, rviz_node, joint_state_publisher_node]
 
 def generate_launch_description():
     # Declare the URDF file name as a launch argument with a default value
